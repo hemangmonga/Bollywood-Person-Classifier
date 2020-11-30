7a1b0a035e28d4f8b91e8b1643eae4c79de67757
Hemang Monga <52912506+hemangmonga@users.noreply.github.com>
2020-11-30 18:18:10 +0530

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,54 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = "http://127.0.0.1:5000/classify_image";
+
+        $.post(url, {
+            image_data: file.dataURL
+        },function(data, status) {
+           
+            
+            console.log(data);
+            document.getElementById("demo").innerHTML = "The Person in the image is "+ data;
+            // document.getElementById("demo").innerHTML = data;
+            if (!data || data.length==0) {
+                $("#resultHolder").hide();
+                $("#divClassTable").hide();                
+                $("#error").show();
+                return;
+            }
+            
+            // dz.removeFile(file);            
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#error").hide();
+    $("#resultHolder").hide();
+    $("#divClassTable").hide();
+
+    init();
+});
